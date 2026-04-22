58b76c439226994c5144e3c650e3931e4054dc23
Patrick <wpatricks68@gmail.com>
2026-04-22 13:26:19 -0300

    insere esqueci senha

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b830237684780697"; }
+/******/ 		__webpack_require__.h = function() { return "1af5abe465aaf331"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
