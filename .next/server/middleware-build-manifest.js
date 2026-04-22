8a7ccec3968b6a7a017a28a2e7b38d976a49667d
Patrick <wpatricks68@gmail.com>
2026-04-22 10:43:42 -0300

    cria sessão de perfil

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-d757b501b8c835e1.js","static/chunks/fd9d1056-5dce5b41981840ea.js","static/chunks/117-e296fd84bf3a8b1d.js","static/chunks/main-app-334354769a050fad.js"],pages:{"/_app":["static/chunks/webpack-d757b501b8c835e1.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-1049dc3f9ec92621.js","static/chunks/pages/_app-15e2daefa259f0b5.js"],"/_error":["static/chunks/webpack-d757b501b8c835e1.js","static/chunks/framework-00a8ba1a63cfdc9e.js","static/chunks/main-1049dc3f9ec92621.js","static/chunks/pages/_error-28b803cb2479b966.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
